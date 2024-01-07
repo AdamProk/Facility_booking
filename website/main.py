f89b9ae46ac977f5dd7c5f25d8e0b2091177e46c
Saiieq <dmartynskix3@gmail.com>
2024-01-07 17:50:56 +0100

    Dodawanie obiektow i rozdzielenie loginu

--- website/main.py
+++ website/main.py
@@ -34,8 +34,11 @@ def index():
         LOGGER.error(e)
         data = []
 
-    return render_template("index.html", data=data)
+    return render_template("home.html", data=data)
 
+@app.route("/login", methods=["GET"])
+def login_site():
+    return render_template("login.html")
 
 @app.route("/login", methods=["POST"])
 def login():
@@ -55,6 +58,114 @@ def logout():
     session.pop("token", None)
     return redirect(url_for("index"))
 
+@app.route("/addfacility", methods=['GET'])
+def addfacility_site():
+    try:
+        data = API.make_request(
+            API.METHOD.GET,
+            API.DATA_ENDPOINT.FACILITY_TYPE,
+        )
+    except API.APIError as e:
+        LOGGER.error(e)
+        data = []
+    return render_template("addfacility.html", data=data)
+
+@app.route("/add_facility", methods=['POST'])
+def add_facility():
+  #id_facility = int(request.form.get("id_facility"))
+    if not any(city['name'] == str(request.form.get("city")) for city in API.make_request(API.METHOD.GET, API.DATA_ENDPOINT.CITY,)):
+        API.make_request(API.METHOD.POST, API.DATA_ENDPOINT.CITY, body = { "name": str(request.form.get("city"))},)
+    
+    if not any(state['name'] == str(request.form.get("state")) for state in API.make_request(API.METHOD.GET, API.DATA_ENDPOINT.STATE,)):
+        API.make_request(API.METHOD.POST, API.DATA_ENDPOINT.STATE, body = { "name": str(request.form.get("state"))},)
+    facility_name = str(request.form.get("name"))
+    description =  str(request.form.get("description"))
+    price_hourly = int(request.form.get("price_hourly"))
+    id_facility_type = int(request.form.get("id_facility_type"))
+    city_name = str(request.form.get("city")).capitalize()
+    street_name = str(request.form.get("street_name")).capitalize()
+    state_name = str(request.form.get("state"))
+    building_no = int(request.form.get("building_number"))
+    postal_code = str(request.form.get("postal_code"))
+
+    cities = API.make_request(API.METHOD.GET, API.DATA_ENDPOINT.CITY)
+
+    try:
+        for city in API.make_request(API.METHOD.GET, API.DATA_ENDPOINT.CITY):
+            print(city['name'])
+            if city['name'] == city_name:
+                id_city = city['id_city']
+                print(id_city)
+        if id_city == None:
+            raise TypeError("City not found")
+    except Exception as e:
+        LOGGER.error(e)
+
+    try:
+        for states in API.make_request(API.METHOD.GET, API.DATA_ENDPOINT.STATE):
+            if states['name'] == state_name:
+                id_state = states['id_state']
+        if id_state == None:
+            raise TypeError("State not found")
+    except Exception as e:
+        LOGGER.error(e)
+
+    address_dic_criteria =  {
+    "city": {
+      "id_city": id_city,
+      "name": city_name,
+    },
+    "state": {
+      "id_state": id_state,
+      "name": state_name,
+    },
+    "street_name": street_name,
+    "building_number": building_no,
+    "postal_code": postal_code
+    }
+
+    address_exists = any(all(d.get(key) == value for key, value in address_dic_criteria.items()) for d in API.make_request(API.METHOD.GET, API.DATA_ENDPOINT.ADDRESS))
+
+    if not address_exists:
+        try:
+            API.make_request(API.METHOD.POST, API.DATA_ENDPOINT.ADDRESS, body ={       
+                "city_name": city_name,
+                "state_name": state_name,
+                "street_name": street_name,
+                "building_number": building_no,
+                "postal_code": postal_code,
+            })
+        except API.APIError as e:
+            LOGGER.error(e)
+
+    try:  
+        for address in API.make_request(API.METHOD.GET, API.DATA_ENDPOINT.ADDRESS,):
+            print(address)
+            print(address_dic_criteria)
+            if all(address[key] == address_dic_criteria[key] for key in address_dic_criteria):
+                id_address = address['id_address']
+        if id_address == None:
+            raise TypeError("Address not found")
+    except Exception as e:
+        LOGGER.error(e)
+
+    try:
+        API.make_request(
+            API.METHOD.POST,
+            API.DATA_ENDPOINT.FACILITY,
+            body = {
+                "name": facility_name,
+                "description": description,
+                "price_hourly": price_hourly,
+                "id_facility_type": id_facility_type,
+                "id_address": id_address,
+                "id_company": 1,
+                "ids_open_hours": [1], #zastanowic sie
+            },)
+    except API.APIError as e:
+        LOGGER.error(e)
+
+    return redirect(url_for("index"))
 
 @app.route("/upload_facility_image", methods=["POST"])
 def upload_facility_image():
